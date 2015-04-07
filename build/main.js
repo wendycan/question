226b9fe77f665e2eb276c375076110d30fb71462
wendy <wendycaner@gmail.com>
2015-04-07 15:33:35 +0800

    Add options

--- build/main.js
+++ build/main.js
@@ -1,3 +1,9 @@
+window.toLetters = function(num) {
+  var mod = num % 26,
+      pow = num / 26 | 0,
+      out = mod ? String.fromCharCode(64 + mod) : (--pow, 'Z');
+  return pow ? toLetters(pow) + out : out;
+}
 var Question = React.createClass({displayName: "Question",
   render: function() {
     var rawMarkup = this.props.children.toString();
@@ -9,7 +15,25 @@ var Question = React.createClass({displayName: "Question",
     );
   }
 });
+var OptionForm = React.createClass({displayName: "OptionForm",
+  handleOptionSubmit: function() {
+    var option = React.findDOMNode(this.refs.option).value.trim();
+    this.props.onOptionSubmit({option: option});
+    React.findDOMNode(this.refs.option).value = '';
+  },
+  render: function() {
+    return (
+      React.createElement("div", null, 
+        React.createElement("input", {type: "text", className: "form-option", placeholder: "Option", ref: "option"}), 
+        React.createElement("div", {className: "btn btn-default", id: "create-option", onClick: this.handleOptionSubmit}, "添加")
+      )
+    );
+  }
+});
 var QuestionForm = React.createClass({displayName: "QuestionForm",
+  getInitialState: function() {
+    return {options: []};
+  },
   handleSubmit: function(e) {
     e.preventDefault();
     var title = React.findDOMNode(this.refs.title).value.trim();
@@ -18,33 +42,73 @@ var QuestionForm = React.createClass({displayName: "QuestionForm",
       return;
     }
     this.props.onQuestionSubmit({title: title, answer: answer});
-    React.findDOMNode(this.refs.title).value = '';
-    React.findDOMNode(this.refs.answer).value = '';
     $('#question-form').empty();
+  },
+  handleOptionSubmit: function(option) {
+    var options = this.state.options;
+    options.push(option);
+    this.setState({options: options});
+  },
+  addOption: function() {
+    React.render(
+      React.createElement(OptionForm, {onOptionSubmit: this.handleOptionSubmit}),
+      document.getElementById('question-options')
+    );
   },
   render: function() {
     return (
       React.createElement("form", {className: "questionForm", onSubmit: this.handleSubmit}, 
-        React.createElement("div", {class: "form-group"}, 
+        React.createElement("div", {className: "form-group"}, 
           React.createElement("label", null, "问题"), 
           React.createElement("input", {type: "text", className: "form-control", placeholder: "Title", ref: "title"})
         ), 
-        React.createElement("div", {class: "form-group"}, 
+        React.createElement("div", {className: "form-group"}, 
+          React.createElement("label", null, "选项"), 
+          React.createElement("button", {className: "btn btn-default", onClick: this.addOption}, "添加"), 
+          React.createElement("div", {id: "question-options"}), 
+          React.createElement(Options, {options: this.state.options})
+        ), 
+
+        React.createElement("div", {className: "form-group"}, 
+          React.createElement("label", {className: "checkbox-inline"}, 
+            React.createElement("input", {type: "checkbox", id: "inlineCheckbox1", value: "option1"}), " 1"
+          ), 
+          React.createElement("label", {className: "checkbox-inline"}, 
+            React.createElement("input", {type: "checkbox", id: "inlineCheckbox1", value: "option1"}), " 2"
+          ), React.createElement("label", {className: "checkbox-inline"}, 
+            React.createElement("input", {type: "checkbox", id: "inlineCheckbox1", value: "option1"}), " 3"
+          )
+        ), 
+        React.createElement("div", {className: "form-group"}, 
           React.createElement("label", null, "答案"), 
-          React.createElement("input", {type: "text", className: "form-control", placeholder: "Answer", ref: "answer"})
+          React.createElement("textarea", {className: "form-control", rows: "3", placeholder: "Answer", ref: "answer"})
         ), 
         React.createElement("button", {type: "submit", className: "btn btn-default"}, "添加")
       )
     );
   }
 });
+var Options = React.createClass({displayName: "Options",
+  render: function() {
+    var optionsNodes = this.props.options.map(function(option, index) {
+      console.log(index);
+      return (
+        React.createElement("div", {title: option.title, key: index}, 
+          toLetters(index + 1), " ", option.option
+        )
+      );
+    });
+    return (
+      React.createElement("div", {className: "optionsList"}, 
+        optionsNodes
+      )
+    )
+  }
+});
 var Questions = React.createClass({displayName: "Questions",
   render: function() {
     var questionNodes = this.props.data.map(function(question, index) {
       return (
-        // `key` is a React-specific concept and is not mandatory for the
-        // purpose of this tutorial. if you're curious, see more here:
-        // http://facebook.github.io/react/docs/multiple-components.html#dynamic-children
         React.createElement(Question, {title: question.title, key: index}, 
           question.answer
         )
@@ -73,9 +137,19 @@ var ContentBox = React.createClass({displayName: "ContentBox",
       document.getElementById('question-form')
     );
   },
+  generateHTML: function() {
+    console.log(this.state.data);
+  },
   render: function() {
     return (React.createElement("div", {className: "question-box"}, 
-      React.createElement("div", {className: "btn btn-default", onClick: this.newQuestion}, "Add Question"), 
+      React.createElement("div", {className: "row"}, 
+        React.createElement("div", {className: "col-md-1"}, 
+          React.createElement("div", {className: "btn btn-default", onClick: this.newQuestion}, "添加问题")
+        ), 
+        React.createElement("div", {className: "col-md-1"}, 
+          React.createElement("div", {className: "btn btn-default", onClick: this.generateHTML}, "生成 HTML")
+        )
+      ), 
       React.createElement("div", {id: "question-form"}), 
       React.createElement(Questions, {data: this.state.data})
     )
--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+window.toLetters = function(num) {
+  var mod = num % 26,
+      pow = num / 26 | 0,
+      out = mod ? String.fromCharCode(64 + mod) : (--pow, 'Z');
+  return pow ? toLetters(pow) + out : out;
+}
 var Question = React.createClass({
   render: function() {
     var rawMarkup = this.props.children.toString();
@@ -9,7 +15,25 @@ var Question = React.createClass({
     );
   }
 });
+var OptionForm = React.createClass({
+  handleOptionSubmit: function() {
+    var option = React.findDOMNode(this.refs.option).value.trim();
+    this.props.onOptionSubmit({option: option});
+    React.findDOMNode(this.refs.option).value = '';
+  },
+  render: function() {
+    return (
+      <div>
+        <input type="text" className="form-option" placeholder="Option" ref="option" />
+        <div className="btn btn-default" id="create-option" onClick={this.handleOptionSubmit}>添加</div>
+      </div>
+    );
+  }
+});
 var QuestionForm = React.createClass({
+  getInitialState: function() {
+    return {options: []};
+  },
   handleSubmit: function(e) {
     e.preventDefault();
     var title = React.findDOMNode(this.refs.title).value.trim();
@@ -18,33 +42,73 @@ var QuestionForm = React.createClass({
       return;
     }
     this.props.onQuestionSubmit({title: title, answer: answer});
-    React.findDOMNode(this.refs.title).value = '';
-    React.findDOMNode(this.refs.answer).value = '';
     $('#question-form').empty();
+  },
+  handleOptionSubmit: function(option) {
+    var options = this.state.options;
+    options.push(option);
+    this.setState({options: options});
+  },
+  addOption: function() {
+    React.render(
+      <OptionForm onOptionSubmit={this.handleOptionSubmit}/>,
+      document.getElementById('question-options')
+    );
   },
   render: function() {
     return (
       <form className="questionForm" onSubmit={this.handleSubmit}>
-        <div class="form-group">
+        <div className="form-group">
           <label>问题</label>
           <input type="text" className="form-control" placeholder="Title" ref="title" />
         </div>
-        <div class="form-group">
+        <div className="form-group">
+          <label>选项</label>
+          <button className="btn btn-default" onClick={this.addOption}>添加</button>
+          <div id="question-options"></div>
+          <Options options={this.state.options}></Options>
+        </div>
+
+        <div className="form-group">
+          <label className="checkbox-inline">
+            <input type="checkbox" id="inlineCheckbox1" value="option1" /> 1
+          </label>
+          <label className="checkbox-inline">
+            <input type="checkbox" id="inlineCheckbox1" value="option1" /> 2
+          </label><label className="checkbox-inline">
+            <input type="checkbox" id="inlineCheckbox1" value="option1" /> 3
+          </label>
+        </div>
+        <div className="form-group">
           <label>答案</label>
-          <input type="text" className="form-control" placeholder="Answer" ref="answer" />
+          <textarea className="form-control" rows="3" placeholder="Answer" ref="answer"></textarea>
         </div>
         <button type="submit" className="btn btn-default">添加</button>
       </form>
     );
   }
 });
+var Options = React.createClass({
+  render: function() {
+    var optionsNodes = this.props.options.map(function(option, index) {
+      console.log(index);
+      return (
+        <div title={option.title} key={index}>
+          {toLetters(index + 1)} {option.option}
+        </div>
+      );
+    });
+    return (
+      <div className="optionsList">
+        {optionsNodes}
+      </div>
+    )
+  }
+});
 var Questions = React.createClass({
   render: function() {
     var questionNodes = this.props.data.map(function(question, index) {
       return (
-        // `key` is a React-specific concept and is not mandatory for the
-        // purpose of this tutorial. if you're curious, see more here:
-        // http://facebook.github.io/react/docs/multiple-components.html#dynamic-children
         <Question title={question.title} key={index}>
           {question.answer}
         </Question>
@@ -73,9 +137,19 @@ var ContentBox = React.createClass({
       document.getElementById('question-form')
     );
   },
+  generateHTML: function() {
+    console.log(this.state.data);
+  },
   render: function() {
     return (<div className="question-box">
-      <div className="btn btn-default" onClick={this.newQuestion}>Add Question</div>
+      <div className="row">
+        <div className="col-md-1">
+          <div className="btn btn-default" onClick={this.newQuestion}>添加问题</div>
+        </div>
+        <div className="col-md-1">
+          <div className="btn btn-default" onClick={this.generateHTML}>生成 HTML</div>
+        </div>
+      </div>
       <div id="question-form"></div>
       <Questions data={this.state.data} />
     </div>
